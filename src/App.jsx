67bff6c26614febd3b7bb9da7b989522277923a4
evanmarshall-dev <me@evanmarshall.dev>
2025-07-15 10:13:08 -0300

    project(emojipedia): Setup initial app to demo mapping data in react.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,47 @@
 import "./App.css";
-import ContactCard from "./components/ContactCard";
 import Footer from "./components/Footer";
 import Heading from "./components/Heading";
-import contacts from "./data/contacts";
-
-function createCard(contact) {
-  return (
-    <ContactCard
-      name={contact.name}
-      img={contact.imgUrl}
-      tel={contact.phone}
-      email={contact.email}
-      key={contact.id}
-      id={contact.id}
-    />
-  );
-}
 
 function App() {
   return (
     <>
       <main>
         <Heading />
-        <section className="contactContainer">
-          {contacts.map(createCard)}
-        </section>
+        <dl className="description">
+          <div className="description__container">
+            <dt>
+              <span aria-label="Tense Bicep">💪</span>
+              <span>Tense Bicep</span>
+            </dt>
+            <dd>
+              “You can do that!” or “I feel strong!” Arm with tense biceps. Also
+              used in connection with doing sports, e.g. at the gym.
+            </dd>
+          </div>
+          <div className="description__container">
+            <dt>
+              <span aria-label="Prayer">🙏</span>
+              <span>Prayer</span>
+            </dt>
+            <dd>
+              Two hands pressed together. Is currently very introverted, saying
+              a prayer, or hoping for enlightenment. Is also used as a “high
+              five” or to say thank you.
+            </dd>
+          </div>
+          <div className="description__container">
+            <dt>
+              <span aria-label="Celebration">🙌</span>
+              <span>Celebration</span>
+            </dt>
+            <dd>
+              Hallelujah! Hands are stretched upwards to celebrate. Is having a
+              party, dancing wildly and friskily. Is in a good mood or having a
+              lot of fun. Can also stand for “Banzai”, a Japanese cheer that
+              brings luck and joy for 10,000 years.
+            </dd>
+          </div>
+        </dl>
       </main>
       <Footer />
     </>
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,7 +2,7 @@ const Heading = () => {
   return (
     <header className="heading">
       {/* <h1>UI Component Library</h1> */}
-      <h1>My Contacts</h1>
+      <h1>Emojipedia</h1>
     </header>
   );
 };
