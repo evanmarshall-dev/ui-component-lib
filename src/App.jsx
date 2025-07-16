389485dc4589bbb6435a471f96cff74395508be4
evanmarshall-dev <me@evanmarshall.dev>
2025-07-16 00:22:30 -0300

    stripe(interview problem): Finished problem with   the help of AI. I do not completely understand   the solution (i.e. useMemo and ...Object.values)   but I do understand the conditional rendering of   list item styles in the ListItems component.
    todo: Study React and array iterator methods more
      and then revisit question.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Heading from "./components/Heading";
@@ -9,16 +10,22 @@ import transactions from "./data/transactions";
 // Highlight even if several instances in array.
 
 function App() {
-  let customerTotals = {};
+  const customerTotals = useMemo(() => {
+    // Use reduce to create an object with customer totals.
+    return transactions.reduce((totals, currentTx) => {
+      const { customer, amount } = currentTx;
+      // If the customer isn't in our totals object, init with 0 then add the current amount.
+      totals[customer] = (totals[customer] || 0) + amount;
+      return totals;
+    }, {});
+  }, []); // Empty dependency array because transactions is static.
 
-  customerTotals = transactions.reduce((totals, currentTx) => {}, {});
-
+  // Find the highest total amount from all customers.
+  const highestTotal = useMemo(
+    () => Math.max(...Object.values(customerTotals)),
+    [customerTotals]
+  );
   console.log(customerTotals);
-
-  // const customers = transactions.map((idx) => {
-  //   console.log(idx);
-  //   // return {idx.customer} {idx.amount}
-  // });
 
   return (
     <>
@@ -26,11 +33,15 @@ function App() {
         <Heading />
         <h2>Customer Transactions</h2>
         {transactions.map((item) => {
+          // Check if the current item's customer is a top spender.
+          const isHighest = customerTotals[item.customer] === highestTotal;
           return (
             <ListItems
               key={item.id}
               customer={item.customer}
               amount={item.amount}
+              // Can now use the prop in ListItems component to apply a style.
+              isHighest={isHighest}
             />
           );
         })}
--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -1,7 +1,10 @@
-const ListItems = ({ customer, amount }) => {
+const ListItems = ({ customer, amount, isHighest }) => {
+  // Conditionally apply style if isHighest is true.
+  const listItemStyle = isHighest ? { backgroundColor: "yellow" } : {};
+
   return (
     <ul>
-      <li>
+      <li style={listItemStyle}>
         {customer} : {amount}
       </li>
     </ul>
